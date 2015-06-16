2ce1610c1628e944a20dd626291180930b7d40fb
hjq <hjq@example.com>
2015-06-16 15:35:20 +0800

    Initial Commit

---
+++ pagecontroldemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pagecontroldemo
+//
+//  Created by roboca on 15/6/16.
+//  Copyright (c) 2015年 roboca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
